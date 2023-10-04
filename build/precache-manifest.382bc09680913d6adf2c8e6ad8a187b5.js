ed0c1275ad207791973dc99b715482d7f87353ef
Ron Ho <ande1218@gmail.com>
2023-10-04 10:14:43 +0800

    feat: free production

---
+++ build/precache-manifest.382bc09680913d6adf2c8e6ad8a187b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebebab1005bc4c4ef4bd017aeb832725",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b38f4c4dddf0ba1c4bfa",
+    "url": "./static/css/main.89ae3c64.chunk.css"
+  },
+  {
+    "revision": "c6811e9df5345dc5c092",
+    "url": "./static/js/2.4ddd50c7.chunk.js"
+  },
+  {
+    "revision": "cccde7cac421bbd50567c4a9194393de",
+    "url": "./static/js/2.4ddd50c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38f4c4dddf0ba1c4bfa",
+    "url": "./static/js/main.a83ed899.chunk.js"
+  },
+  {
+    "revision": "0e23c734c44c543dbfce",
+    "url": "./static/js/runtime-main.cfb66ff0.js"
+  }
+]);
